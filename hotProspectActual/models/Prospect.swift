f9012f936cd8851f4f6f06e3082fc320c8c03fa7
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2020-06-28 16:01:51 -0700

    project ready

---
+++ hotProspectActual/models/Prospect.swift
@@ -0,0 +1,48 @@
+//
+//  Prospect.swift
+//  hotProspectActual
+//
+//  Created by Aldiyar Batyrbekov on 6/28/20.
+//  Copyright © 2020 Aldiyar Batyrbekov. All rights reserved.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    let id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    static let saveKey = "SavedData"
+    @Published private(set) var people: [Prospect]
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+        self.people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+    
+    func add(prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+}
---
+++ hotProspectActual/views/ProspectsView.swift
@@ -0,0 +1,133 @@
+//
+//  ProspectsView.swift
+//  hotProspectActual
+//
+//  Created by Aldiyar Batyrbekov on 6/27/20.
+//  Copyright © 2020 Aldiyar Batyrbekov. All rights reserved.
+//
+
+import SwiftUI
+import CodeScanner
+import UserNotifications
+
+enum FilterType {
+    case none, contacted, uncontacted
+}
+struct ProspectsView: View {
+    @State private var isShowingScanner = false
+    @EnvironmentObject var prospects: Prospects
+    let filter: FilterType
+    
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                    .contextMenu {
+                        Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted") {
+                            self.prospects.toggle(prospect)
+                        }
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                self.addNotification(for: prospect)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(title)
+            .navigationBarItems(trailing: Button(action: {
+                self.isShowingScanner = true
+            }, label: {
+                Image(systemName: "qrcode.viewfinder")
+                Text("Scan")
+            }))
+        }
+        .sheet(isPresented: $isShowingScanner, content: {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "Test\ntest@test.com", completion: self.handleScan)
+        })
+    }
+    
+    func handleScan(result: Result<String, CodeScannerView.ScanError>) {
+        self.isShowingScanner = false
+        switch result {
+        case .success(let code):
+            let details = code.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            
+            self.prospects.add(prospect: person)
+        case .failure(let error):
+            print("Scanning failed")
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+            
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Won't show the notifications")
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
